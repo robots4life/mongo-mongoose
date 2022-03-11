fbfb744c3454ec7ad37a85d12f7f11e8331982ea
robots4life <34193243+robots4life@users.noreply.github.com>
2022-03-11 08:55:28 +0100

    init

---
+++ src/routes/index.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+
+import { MongoClient } from 'mongodb';
+
+async function run() {
+	try {
+		const uri = 'mongodb://localhost:27017';
+
+		// Create a new MongoClient
+		const client = new MongoClient(uri);
+
+		await client.connect();
+
+		console.log(client);
+
+		const conn = mongoose.createConnection().setClient(client);
+		// MongooseError: Must call `setClient()` with an instance of MongoClient
+
+		/*
+        Argument of type 'import("/shared/httpd/mongo-mongoose/node_modules/mongodb/mongodb").MongoClient' is not assignable to parameter of type 'import("/shared/httpd/mongo-mongoose/node_modules/mongoose/node_modules/mongodb/mongodb").MongoClient'.
+        The types of 'options.credentials' are incompatible between these types.
+        Type 'import("/shared/httpd/mongo-mongoose/node_modules/mongodb/mongodb").MongoCredentials' is not assignable to type 'import("/shared/httpd/mongo-mongoose/node_modules/mongoose/node_modules/mongodb/mongodb").MongoCredentials'.ts(2345)
+        */
+
+		conn.getClient(); // MongoClient { ... }
+		conn.readyState; // 1, means 'CONNECTED'
+	} catch (error) {
+		console.log(error);
+	}
+}
+run();
+
+export async function get() {
+	try {
+		const uri = 'mongodb://localhost:27017';
+
+		// Create a new MongoClient
+		const client = new MongoClient(uri);
+
+		console.log(client);
+
+		await client.connect();
+
+		const conn = mongoose.createConnection().setClient(client);
+		// MongooseError: Must call `setClient()` with an instance of MongoClient
+
+		/*
+        Argument of type 'import("/shared/httpd/mongo-mongoose/node_modules/mongodb/mongodb").MongoClient' is not assignable to parameter of type 'import("/shared/httpd/mongo-mongoose/node_modules/mongoose/node_modules/mongodb/mongodb").MongoClient'.
+        The types of 'options.credentials' are incompatible between these types.
+        Type 'import("/shared/httpd/mongo-mongoose/node_modules/mongodb/mongodb").MongoCredentials' is not assignable to type 'import("/shared/httpd/mongo-mongoose/node_modules/mongoose/node_modules/mongodb/mongodb").MongoCredentials'.ts(2345)
+        */
+
+		conn.getClient(); // MongoClient { ... }
+		conn.readyState; // 1, means 'CONNECTED'
+
+		return {
+			status: 200,
+			body: {
+				message: 'ok'
+			}
+		};
+	} catch (error) {
+		console.log(error);
+	}
+}
